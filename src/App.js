adc51076b1d21a85e9799ca449954f426894a007
Felipe Garcia <22felipe95@gmail.com>
2023-02-11 10:02:51 -0400

    Created CampsitesList

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import React from 'react';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
-import CampsiteCard from './features/campsites/CampsiteCard.js';
-import { CAMPSITES } from './app/shared/CAMPSITES';
+import CampsitesList from './features/campsites/CampsitesList';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Navbar dark color='primary' sticky='top' expand='md'>
-        <Container>
-          <NavbarBrand href='/'>
-            <img src={NucampLogo} />
-          </NavbarBrand>
-        </Container>
-      </Navbar>
-      <CampsiteCard campsite={CAMPSITES[0]} />
+      <Header />
+      <CampsitesList />
+      <Footer />
     </div>
   );
 }
