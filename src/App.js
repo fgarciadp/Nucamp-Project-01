8fa7dd171f890c273983e6fc41dbde16dc424458
Felipe Garcia <22felipe95@gmail.com>
2023-02-11 08:48:07 -0400

    Created CampsiteCard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import NucampLogo from './app/assets/img/logo.png';
+import CampsiteCard from './features/campsites/CampsiteCard.js';
+import { CAMPSITES } from './app/shared/CAMPSITES';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      welcome!
+      <Navbar dark color='primary' sticky='top' expand='md'>
+        <Container>
+          <NavbarBrand href='/'>
+            <img src={NucampLogo} />
+          </NavbarBrand>
+        </Container>
+      </Navbar>
+      <CampsiteCard campsite={CAMPSITES[0]} />
     </div>
   );
 }
